1a538ec3bc00dc6f1b91f9976f1f6d1eaaab0bc1
Henrik Kniberg <h@henrikscompany.com>
2017-10-18 19:26:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ USER=`grep -Po '"user":"\K[\w\.-]*' /home/pi/apps/sshtunnel/config/local.json`
 
 echo Opening SSH reverse tunnel to $USER@$HOST:$PORT
 
-ssh -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 $USER@$HOST
+ssh -i /home/pi/.ssh/id_rsa -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 $USER@$HOST
