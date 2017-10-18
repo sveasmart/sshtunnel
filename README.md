ce44165ac57bc787d4849e90809a5be0fe242f12
Henrik Kniberg <h@kniberg.com>
2017-10-18 06:22:35 +0200

    First working version of the tunnel

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# sshtunnel
+# SSH Tunnel
+
+An app that opens an ssh reverse tunnel
+
+Run it using start.sh. It reads host and port from config/local.json.
+
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#Parse the port and host from config/local.json
+PORT=`grep -Po '"port":"\K\d*' config/local.json`
+HOST=`grep -Po '"host":"\K[\w\.]*' config/local.json`
+
+echo Opening SSH reverse tunnel from $HOST:$PORT
+
+ssh -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 pi@$HOST
