db0ec5540d0686a63168c1c4ecc7ca16aeb5bb4c
Henrik Kniberg <h@henrikscompany.com>
2017-10-18 18:36:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 #Parse the port and host from config/local.json
 PORT=`grep -Po '"port":"\K\d*' config/local.json`
 HOST=`grep -Po '"host":"\K[\w\.]*' config/local.json`
+USER=`grep -Po '"user":"\K[\w\.]*' config/local.json`
 
-echo Opening SSH reverse tunnel from $HOST:$PORT
+echo Opening SSH reverse tunnel to $USER@$HOST:$PORT
 
-ssh -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 pi@$HOST
+ssh -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 $USER@$HOST
