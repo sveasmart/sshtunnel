253e751829a98825659adf28495319c19250377b
Henrik Kniberg <h@henrikscompany.com>
2017-10-18 19:00:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #Parse the port and host from config/local.json
-PORT=`grep -Po '"port":"\K\d*' config/local.json`
-HOST=`grep -Po '"host":"\K[\w\.-]*' config/local.json`
-USER=`grep -Po '"user":"\K[\w\.-]*' config/local.json`
+PORT=`grep -Po '"port":"\K\d*' /home/pi/sshtunnel/config/local.json`
+HOST=`grep -Po '"host":"\K[\w\.-]*' /home/pi/sshtunnel/config/local.json`
+USER=`grep -Po '"user":"\K[\w\.-]*' /home/pi/sshtunnel/config/local.json`
 
 echo Opening SSH reverse tunnel to $USER@$HOST:$PORT
 
