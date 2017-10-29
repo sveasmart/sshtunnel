ad48c70b5108b7f690f5e1f991f3537ac358550f
Henrik Kniberg <h@kniberg.com>
2017-10-30 02:07:14 +0700

    switched to autossh (to keep connection alive)

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ PORT=`grep -Po '"port":"\K\d*' /home/pi/apps/sshtunnel/config/local.json`
 HOST=`grep -Po '"host":"\K[\w\.-]*' /home/pi/apps/sshtunnel/config/local.json`
 USER=`grep -Po '"user":"\K[\w\.-]*' /home/pi/apps/sshtunnel/config/local.json`
 
-echo Opening SSH reverse tunnel to $USER@$HOST:$PORT
+echo Opening AutoSSH reverse tunnel to $USER@$HOST:$PORT
 
-ssh -i /home/pi/.ssh/id_rsa -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 $USER@$HOST
+autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -i /home/pi/.ssh/id_rsa -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 $USER@$HOST
