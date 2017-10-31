ca529ab29a59a590fafbad0b7ab39bdeb326f394
Henrik Kniberg <h@kniberg.com>
2017-10-31 18:12:29 +0700

    Starts after network is started. Waits 10 seconds between restarts. Uses autossh if available, and ssh if not.

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ PORT=`grep -Po '"port":"?\K\d*' /home/pi/apps/sshtunnel/config/local.json`
 HOST=`grep -Po '"host":"\K[\w\.-]*' /home/pi/apps/sshtunnel/config/local.json`
 USER=`grep -Po '"user":"\K[\w\.-]*' /home/pi/apps/sshtunnel/config/local.json`
 
-echo Opening AutoSSH reverse tunnel to $USER@$HOST:$PORT
-
-autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -i /home/pi/.ssh/id_rsa -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 $USER@$HOST
+if ! [ -x "$(command -v autossh)" ]; then
+  echo "Using ssh to open reverse tunnel to $USER@$HOST:$PORT"
+  ssh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -i /home/pi/.ssh/id_rsa -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 $USER@$HOST
+else
+  echo "Using autossh to open reverse tunnel to $USER@$HOST:$PORT"
+  autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -i /home/pi/.ssh/id_rsa -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -R $PORT:localhost:22 $USER@$HOST
+fi
